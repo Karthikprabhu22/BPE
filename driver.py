c352b49db46edb41826c41a55fb70892061c46a4
Chase Hansen <cmhansen@ucdavis.edu>
2020-05-11 15:55:41 -0700

    switch flipped

--- driver.py
+++ driver.py
@@ -11,7 +11,7 @@ import corner
 c = bpe.DataContainer()
 c.import_data()
 
-chain = bpe.MCMC(10000, c, include_systematic_errors=True, include_M_prior=False)
+chain = bpe.MCMC(10000, c, include_systematic_errors=True, include_M_prior=True)
 
 # Plotting function
 fig = plt.figure()
